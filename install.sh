0f0825a68d7a2f578c73a2ac7f2cf4afd13f6078
ShadowVMX <92258683+ShadowVMX@users.noreply.github.com>
2023-02-14 13:57:38 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#
+#Instalación de los servicios necesarios.
+#
+#DHCP,DNS,ADDS,VPN,MONITORIZACIÓN,LOGS,HTTP,SMTP,SSH,FTP, etc...
+#
+#
+#
+
+echo "$(tput staf 6)"
+
+figlet Actualización de Sistema:
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Updateando el Sistema ... "
+apt update -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Upgradeando el Sistema ..."
+apt upgrade -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 6)"
+
+figlet Instalando Servicios de Red:
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio DHCP ..."
+apt install isc-dhcp-server -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio DNS ... "
+apt install bind9 -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio SMTP ... "
+apt install postfix -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio FTP ... "
+apt install vsftpd -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio SSH ... "
+apt install openssh-server -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio SMB ... "
+apt install samba -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+
+echo "$(tput setaf 6)"
+
+figlet Instalando Servicios Web:
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servico HTTP Apache2 ... "
+apt install apache2 -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio HTTP nginx ... "
+apt install nginx -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 6)"
+
+figlet Instalando Servicios de Seguridad:
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio VPN ... "
+apt install openvpn -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado."
+sleep 1
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio UFW ..."
+apt install ufw -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado. "
+
+echo "$(tput setaf 6)"
+
+figlet Instalando Servicios DB:
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando servicio MySQL ... "
+apt install mysql-server -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado. "
+sleep 1
+
+echo "$(tput setaf 6)"
+
+figlet Instalando Monitorización:
+
+echo "$(tput setaf 9)[$(tput staf 6)*[$(tput setaf 9)]$(tput setaf 2) Instalando NetGIOS ... "
+apt install nagios4 nagios-nrpe-plugin -y &> /dev/null
+
+echo "$(tput setaf 9)[$(tput staf 3)*[$(tput setaf 9)]$(tput setaf 2) Completado. "
+sleep 1

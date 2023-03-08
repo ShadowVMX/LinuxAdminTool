544cd6e4ce83fb4a901fbb25fae33ba456a2adb9
ShadowVMX <92258683+ShadowVMX@users.noreply.github.com>
2023-03-08 16:24:53 +0100

    Update send.py

--- send.py
+++ send.py
@@ -1,8 +1,18 @@
 import telegram
+import subprocess
+                                         
+# Configura tu bot de Telegram
+bot = telegram.Bot(token='5981434873:AAE1e260sk_8Xu5Gy9N93zodag9WBHuaxOM')
 
-TOKEN = '5981434873:AAE1e260sk_8Xu5Gy9N93zodag9WBHuaxOM'
-bot = telegram.Bot(TOKEN)
+# ID de chat donde quieres enviar los logs
 chat_id = '5088751124'
-message = 'Hola, este es un mensaje de mi bot de Tele'
-bot.send_message(chat_id=chat_id, text=message)
-                                                   
+
+# Ejecuta el comando 'tail' en la consola para obtener las últimas 10 líneas del archivo de registro del DHCP
+process = subprocess.Popen(['tail', '-n', '10', '/var/log/syslog'], stdout=subprocess.PIPE)
+output, error = process.communicate()
+
+# Codifica la salida como una cadena de texto
+logs = output.decode('utf-8')
+
+# Envía los logs al bot de Telegram
+bot.send_message(chat_id=chat_id, text=logs)

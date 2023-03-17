12ca302c6790552c0d760f38ee3b47cc0e17a622
ShadowVMX <92258683+ShadowVMX@users.noreply.github.com>
2023-03-18 00:58:00 +0100

    Update send.py

--- send.py
+++ send.py
@@ -1,35 +1,31 @@
-import telegram
 import subprocess
-from scapy.all import *
+import re
+import requests
 
-# Configura tu bot de Telegram
-bot = telegram.Bot(token='5981434873:AAE1e260sk_8Xu5Gy9N93zodag9WBHuaxOM')
+# Token del bot de Telegram
+TOKEN = '5981434873:AAE1e260sk_8Xu5Gy9N93zodag9WBHuaxOM'
 
-# ID de chat donde quieres enviar los logs
-chat_id = '5088751124'
+# ID del chat donde se enviará el mensaje
+CHAT_ID = '5088751124'
 
-# Función para capturar paquetes DHCP y obtener información relevante
-def dhcp_handler(packet):
-    if DHCP in packet and packet[DHCP].options[0][1] == 5:
-        # Obtener la información de la concesión DHCP
-        hostname = packet[DHCP].options[3][1].decode('utf-8')
-        ip = packet[BOOTP].yiaddr
-        mac = packet[Ether].src
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+# Expresión regular para buscar la información del cliente en el registro DHCP
+regex = r'DHCPACK on (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) to ([^\s]+) \(([\dA-Fa-f]{2}[:-]){5}([\dA-Fa-f]{2})\).*lease (\d+)'
 
-        # Formatear la información y enviarla a través del bot de Telegram
-        message = f'Dispositivo: {hostname}\nIP: {ip}\nMAC: {mac}\nHora de conexión: {timestamp}'
-        bot.send_message(chat_id=chat_id, text=message)
+# Ejecutar el comando para leer el registro DHCP
+output = subprocess.check_output(['cat', '/var/log/syslog | grep "DHCPACK on"'], shell=True)
 
-# Capturar paquetes DHCP en tiempo real
-sniff(filter='udp and (port 67 or 68)', prn=dhcp_handler)
+# Buscar la información del cliente en el registro DHCP
+match = re.search(regex, output.decode())
 
-# Ejecuta el comando 'tail' en la consola para obtener las últimas 10 líneas del archivo de registro del DHCP
-process = subprocess.Popen(['tail', '-n', '10', '/var/log/syslog'], stdout=subprocess.PIPE)
-output, error = process.communicate()
+# Si se encuentra la información del cliente
+if match:
+    ip_address = match.group(1)
+    hostname = match.group(2)
+    mac_address = match.group(3).replace(':', '-')
+    lease_time = match.group(4)
 
-# Codifica la salida como una cadena de texto
-logs = output.decode('utf-8')
+    # Construir el mensaje a enviar al bot de Telegram
+    message = f'Cliente conectado al DHCP:\nIP: {ip_address}\nNombre de equipo: {hostname}\nDirección MAC: {mac_address}\nTiempo de concesión: {lease_time}'
 
-# Envía los logs al bot de Telegram
-bot.send_message(chat_id=chat_id, text=logs)
+    # Enviar el mensaje al bot de Telegram
+    requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}')
